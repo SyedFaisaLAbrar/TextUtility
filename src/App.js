3d9d6f9541d7ee988e4111a66421e62028db94fd
Syed Faisal Abrar <syedfaisalabrar100@gmail.com>
2023-09-30 12:10:44 +0500

    Renewed
    Updated Files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import './App.css'
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar'
 import Textform from './components/Textform';
 import Alert from './components/Alert';
-// import { BrowserRouter as Router,Routes, Route } from 'react-router-dom
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 
 
 function App() {
@@ -19,11 +19,10 @@ function App() {
     }, 2000);
   }
 
-  const [mode, setMode] = useState('light')
-  const [color, setColor] = useState('dark')
-  const [bgcolor, setBgColor] = useState('light')
+  const [mode, setMode] = useState('')
+  const [color, setColor] = useState('')
+  const [bgcolor, setBgColor] = useState('');
 
-  
   const shiftToRed = () => {
     setMode('red')
     setColor('white')
@@ -86,28 +85,52 @@ function App() {
     else if(destMode === 'light'){
       shiftToWhite()
     }
+    
+
+    // if (destMode === 'dark' && mode === 'light') {
+    //   setMode('dark')
+    //   setColor('white')
+    //   setBgColor('#1D2951')
+    //   document.body.style.backgroundColor = '#1D2951'
+    //   showAlert('This is Dark Mode', 'success')
+    //   document.title = 'Textutiles - Dark Mode'
+    // }
+    // else if (destMode === 'red' && mode === 'light') {
+      // setMode('red')
+      // setColor('white')
+      // setBgColor('#3f0000')
+      // document.body.style.backgroundColor = '#3f0000'
+      // showAlert('This is Red Mode', 'success')
+      // document.title = 'Textutiles - Red Mode'
+    // }
+    // else if ((destMode === 'red' && mode === 'red') || (destMode === 'dark' && mode === 'dark')) {
+    //   setMode('light')
+    //   setColor('black')
+    //   setBgColor('white')
+    //   document.body.style.backgroundColor = 'white'
+    //   showAlert('This is Light Mode', 'success')
+    //   document.title = 'Textutiles - Light Mode'
+    // }
   }
 
 
 
   return (
     <>
-      {/* <Router> */}
+      <Router>
         <Navbar title="Text Utilities" aboutTitle="About Us" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
-        <div className="container my-5">
-        {/* <Routes> */}
+        <div className="container my-2">
+        <Routes>
           {/* Its always neccessary to add exact before path. Because react only do partial matching of url
                   like     users/ 
                            users/home  ---->it will render users/ only due to partial matching
           */}
-          {/* <Route exact path='/' element={}/> */}
-          <Textform heading="Enter Text To Analyze" mode={mode} showAlert={showAlert} color={color} bgcolor={bgcolor} />
-          {/* <Route exact path='/about' element={}/> */}
-          {/* <About/> */}
-        {/* </Routes>   */}
+          <Route exact path='/' element={<Textform heading="Enter Text To Analyze" mode={mode} showAlert={showAlert} color={color} bgcolor={bgcolor} />}/>
+          <Route exact path='/about' element={<About mode={mode}/>}/>
+        </Routes>  
         </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
